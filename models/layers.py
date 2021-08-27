c6f9ae5eb8a90ee07a7d012de45219907f71c93a
Skaftenicki <skaftenicki@gmail.com>
2021-08-27 13:58:09 +0200

    small model

--- models/layers.py
+++ models/layers.py
@@ -1,6 +1,14 @@
 import torch
 from torch import nn, Tensor
 
+class AdditiveRegulizer(nn.Module):
+    def __init__(self, epsilon: float = 1e-6):
+        self.epsilon = epsilon
+        
+    def forward(self, x: Tensor) -> Tensor:
+        return x + self.epsilon
+
+
 class Reshape(nn.Module):
     def __init__(self, dims):
         super().__init__()
--- models/small_vae.py
+++ models/small_vae.py
@@ -1,11 +1,44 @@
-from torch import nn
+from torch import nn, Tensor
+from utils import AdditiveRegulizer
 
 
 class Encoder(nn.Module):
-    pass
+    def __init__(self, latent_size: int = 2, n_neurons: int = 50):
+        self.encoder = nn.Sequential(
+            nn.Linear(4, n_neurons),
+            nn.LeakyReLU(),
+        )
+        self.encoder_mu = nn.Linear(n_neurons, latent_size)
+        
+        self.encoder_std = nn.Sequential(
+            nn.Linear(n_neurons, latent_size),
+            nn.Softplus(),
+            AdditiveRegulizer()
+        )
 
-class Decoder(nn.Module):
-    pass
+    def forward(self, x: Tensor) -> Tensor:
+        h = self.encoder(x)
+        return self.encoder_mu(h), self.encoder_std(h)
+
+
+class DecoderNormal(nn.Module):
+    def __init__(self, latent_size: int = 2, n_neurons: int = 50):
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_size, n_neurons),
+            nn.ReLU(),
+        )
+        self.decoder_mu = nn.Linear(n_neurons, 4)
+        self.decoder_std = nn.Sequential(
+            nn.Linear(n_neurons, 4),
+            nn.Softplus(),
+            AdditiveRegulizer()
+        )
+        
+    def forward(self, z: Tensor) -> Tensor:
+        h = self.decoder(z)
+        return self.decoder_mu(h), self.decoder_std(h)
+        
+    
 
 class VAE(nn.Module):
     pass
