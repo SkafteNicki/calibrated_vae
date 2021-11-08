dc2a22b61d3689aa49bd77e28a70c9ec41533047
SkafteNicki <skaftenicki@gmail.com>
2021-11-08 10:30:02 +0100

    cleanup

--- models/__init__.py
+++ models/__init__.py
@@ -2,11 +2,10 @@ from .double_ensample import DEVAE
 from .ensample import EVAE
 from .mc_dropout import MCVAE
 from .mixer_ensample import MEVAE
-from .normal_vae import NVAE
 from .vae import VAE
 
 
 def get_model(name: str):
-    return {"VAE": VAE, "NVAE": NVAE, "MCVAE": MCVAE, "EVAE": EVAE, "DEVAE": DEVAE, "MEVAE": MEVAE}[
+    return {"VAE": VAE, "MCVAE": MCVAE, "EVAE": EVAE, "DEVAE": DEVAE, "MEVAE": MEVAE}[
         name.upper()
     ]
